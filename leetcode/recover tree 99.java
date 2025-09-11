cceca4fef1d8af9d5b83853b6fef353ae0668682
daksh0909152 <2k23.aids2310779@gmail.com>
2025-09-11 10:03:06 +0530

    Create recover tree 99.java

---
+++ leetcode/recover tree 99.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void recoverTree(TreeNode root) {
+        TreeNode first = null, second = null, prev = null;
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (curr.left == null) {
+                // Detect swapped nodes
+                if (prev != null && prev.val > curr.val) {
+                    if (first == null) first = prev;
+                    second = curr;
+                }
+                prev = curr;
+                curr = curr.right;
+            } else {
+                // Morris Traversal: find inorder predecessor
+                TreeNode pred = curr.left;
+                while (pred.right != null && pred.right != curr) {
+                    pred = pred.right;
+                }
+                if (pred.right == null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                } else {
+                    pred.right = null;
+                    // Detect swapped nodes
+                    if (prev != null && prev.val > curr.val) {
+                        if (first == null) first = prev;
+                        second = curr;
+                    }
+                    prev = curr;
+                    curr = curr.right;
+                }
+            }
+        }
+        // Swap values of the two nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+}
