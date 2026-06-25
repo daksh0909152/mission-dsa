7d9f8d51f0d2f4536cf276730244eb8796995e95
daksh0909152 <2k23.aids2310779@gmail.com>
2026-06-25 14:02:02 +0530

    Time: 9 ms (98.66%), Space: 50.3 MB (18.97%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,38 +1,30 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int minweight = 0;
-        int maxweight = 0;
-
-for (int weight : weights) {
-    minweight = Math.max(minweight, weight);
-    maxweight += weight;
-}
+    public int shipWithinDays(int[] weights, int D) {
+        int minCap=0;
+        int maxCap=0;
+        for(int weight:weights){
+            minCap = Math.max(minCap,weight);
+            maxCap += weight;
+        }
         //binary search
-        while(minweight<maxweight){
-            int mid= minweight + (maxweight-minweight)/2;
-            if(CapacityToShipPackages(weights,days,mid))
-            maxweight= mid;
+        while(minCap < maxCap){
+            int mid= minCap + (maxCap-minCap)/2;
+            // try to ship with mid capacity
+            int days=1;
+            int sum=0;
+            for(int weight:weights){
+                if(sum + weight> mid){
+                days++;
+                sum=0;
+            }
+            sum += weight;
+        }
+        //If more days are required increase capacity
+        if(days>D)
+            minCap= mid+1;
             else
-            minweight= mid+1;
+            maxCap= mid;
         }
-        return minweight;
-        
-    }
-   private boolean CapacityToShipPackages(int[] weights, int days, int capacity) {
-    int day = 1;          // pehla day
-    int currentLoad = 0;  // current day ka load
-
-    for (int weight : weights) {
-
-        // agar current package add karne se capacity exceed ho rahi hai
-        if (currentLoad + weight > capacity) {
-            day++;              // naya day start
-            currentLoad = 0;
-        }
-
-        currentLoad += weight;
-    }
-
-    return day <= days;
-}
+    return minCap;   
+   }
 }
