07a838a2fa7f384ac489fb6c5dc0be4307f9536d
daksh0909152 <2k23.aids2310779@gmail.com>
2026-07-09 21:05:56 +0530

    Time: 11 ms (34.65%), Space: 166 MB (31.68%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    class DSU {
+        int[] parent;
+        int[] rank;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); // Path Compression
+            }
+            return parent[x];
+        }
+
+        void union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+
+            if (pa == pb) return;
+
+            if (rank[pa] < rank[pb]) {
+                parent[pa] = pb;
+            } else if (rank[pa] > rank[pb]) {
+                parent[pb] = pa;
+            } else {
+                parent[pb] = pa;
+                rank[pa]++;
+            }
+        }
+    }
+
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+
+        DSU dsu = new DSU(n);
+
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(nums[i] - nums[i - 1]) <= maxDiff) {
+                dsu.union(i, i - 1);
+            }
+        }
+
+        boolean[] ans = new boolean[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = dsu.find(queries[i][0]) == dsu.find(queries[i][1]);
+        }
+
+        return ans;
+    }
+}
