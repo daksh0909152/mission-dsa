ab1989183e7622d1b488cbbfd3db9e0c72c2ba6c
daksh0909152 <2k23.aids2310779@gmail.com>
2026-09-08 23:43:10 +0530

    Time: 2 ms (60.52%), Space: 42.9 MB (78.74%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    
+    public boolean backspaceCompare(String s, String t) {
+        
+        return build(s).equals(build(t));
+    }
+    
+    public String build(String str) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : str.toCharArray()) {
+            
+            if (ch == '#') {
+                
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+                
+            } else {
+                stack.push(ch);
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (char ch : stack) {
+            result.append(ch);
+        }
+        
+        return result.toString();
+    }
+}
